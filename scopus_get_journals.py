532aa1c90e163dc8c70adf1aaa8f5a4d247b3d0f
Robert Jaworek <73638117+rbrtjwrk@users.noreply.github.com>
2021-02-18 23:08:51 +0100

    Update scopus_get_journals.py

--- scopus_get_journals.py
+++ scopus_get_journals.py
@@ -1,12 +1,12 @@
 import json
 import requests
 
-def scopus_get_journals(subject=None, count=None):
+def scopus_get_journals(subject_abbrev=None, count=None):
     payload={
             "Accept": "application/json",
             "X-ELS-APIKey": "e5011a9e1eb81eeea51a2abc18793df0"}
     parameters={
-                "subj": subject,
+                "subj": subject_abbrev,
                 "content": "journal",
                 "count": count}
     response=requests.get("https://api.elsevier.com/content/serial/title", headers=payload, params=parameters)
