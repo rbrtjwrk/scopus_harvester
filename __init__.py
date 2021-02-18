79b14bcb6dac1db01ee036a9ee5a2d7c24498b8c
Robert Jaworek <73638117+rbrtjwrk@users.noreply.github.com>
2021-02-18 19:28:50 +0100

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,16 @@
+import requests
+import json
+import pandas as pd
+
+from .scopus_get_journals import scopus_get_journals
+from .response_to_json import response_to_json
+from .file_to_data import file_to_data
+from .scopus_journal_title import scopus_journal_title
+from .scopus_journal_id import scopus_journal_id
+from .scopus_sjr import scopus_sjr
+from .scopus_citescore import scopus_citescore
+from .scopus_iss import parse_scopus_issn_dict, scopus_issn
+from .scopus_subject_area import scopus_subject_area
+from .scopus_subject_area_code import scopus_subject_area_code
+from .scopus_subject_classification import scopus_subject_classification
+from .scopus_journals import scopus_journals
