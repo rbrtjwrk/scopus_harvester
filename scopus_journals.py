5d15d0f369e3d20450fc3a9567ab3b1f76f8eb2b
Robert Jaworek <73638117+rbrtjwrk@users.noreply.github.com>
2021-02-18 19:45:08 +0100

    Update scopus_journals.py

--- scopus_journals.py
+++ scopus_journals.py
@@ -1,3 +1,15 @@
+from scopus_harvesting.scopus_get_journals import scopus_get_journals
+from scopus_harvesting.response_to_json import response_to_json
+from scopus_harvesting.file_to_data import file_to_data
+from scopus_harvesting.scopus_journal_title import scopus_journal_title
+from scopus_harvesting.scopus_journal_id import scopus_journal_id
+from scopus_harvesting.scopus_sjr import scopus_sjr
+from scopus_harvesting.scopus_citescore import scopus_citescore
+from scopus_harvesting.scopus_iss import parse_scopus_issn_dict, scopus_issn
+from scopus_harvesting.scopus_subject_area import scopus_subject_area
+from scopus_harvesting.scopus_subject_area_code import scopus_subject_area_code
+from scopus_harvesting.scopus_subject_classification import 
+
 def scopus_journals(response):
     output=pd.DataFrame()
     output["Journal_Title"]=scopus_journal_title(response)
