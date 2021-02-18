3b77ddb49973589d01cc76d28a45ce2524e3dbfe
Robert Jaworek <73638117+rbrtjwrk@users.noreply.github.com>
2021-02-18 23:41:23 +0100

    Update scopus_subject_classification.py

--- scopus_subject_classification.py
+++ scopus_subject_classification.py
@@ -6,7 +6,9 @@ def scopus_subject_classification(response):
     f=response_to_json(response)
     data=file_to_data(f)
     for _ in range(len(data)):
-        if len(data[_]["subject-area"]) > 0:
+        if "subject-area" not in data[_].keys():
+            output.append(" ")
+        elif len(data[_]["subject-area"]) > 0:
             subject_areas=data[_]["subject-area"]
             subject_areas_abbrevs= [subject_areas[_]["$"] for _ in range(len(subject_areas))]
             output.append(subject_areas_abbrevs)
