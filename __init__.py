c315f19a0fcfcbcb83dd38d31c81e87a41123ee0
Robert Jaworek <73638117+rbrtjwrk@users.noreply.github.com>
2021-02-18 22:38:49 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -9,7 +9,7 @@ from .scopus_journal_title import scopus_journal_title
 from .scopus_journal_id import scopus_journal_id
 from .scopus_sjr import scopus_sjr
 from .scopus_citescore import scopus_citescore
-from .scopus_iss import parse_scopus_issn_dict, scopus_issn
+from .scopus_issn import parse_scopus_issn_dict, scopus_issn
 from .scopus_subject_area import scopus_subject_area
 from .scopus_subject_area_code import scopus_subject_area_code
 from .scopus_subject_classification import scopus_subject_classification
