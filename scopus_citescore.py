5b7451caa751d4fef2cd9dcc99a5fc04507c546d
Robert Jaworek <73638117+rbrtjwrk@users.noreply.github.com>
2021-02-18 19:02:01 +0100

    Create scopus_citescore.py

---
+++ scopus_citescore.py
@@ -0,0 +1,10 @@
+def scopus_citescore(response):
+    output=[]
+    f=response_to_json(response)
+    data=file_to_data(f)
+    for _ in range(len(data)):
+        if "citeScoreYearInfoList" in data[_]:
+            output.append(data[_]["citeScoreYearInfoList"]["citeScoreCurrentMetric"])
+        else:
+            output.append(0.0)
+    return output
