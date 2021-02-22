778930bce5aefd89afeaaab1900b2b25201f9b61
Robert Jaworek <73638117+rbrtjwrk@users.noreply.github.com>
2021-02-22 12:43:19 +0100

    Update multiply_journals.py

--- multiply_journals.py
+++ multiply_journals.py
@@ -5,7 +5,8 @@ def multiply_journals(dataframe, alphabetical_order=True):
         multiply_by=len(row.Subject_Classification)
         if multiply_by > 1:
             dataframe=dataframe.append([row] * (multiply_by-1), sort=False)
-    dataframe=dataframe.drop(columns=["Index"])
     if alphabetical_order==True:
         dataframe=dataframe.sort_values(by="Journal_Title")
+    dataframe=dataframe.reset_index()
+    dataframe=dataframe.drop(columns=["index", "Index"])
     return dataframe
