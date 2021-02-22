d02809fef4785127f867fc845e36e9f5a0d6719f
Robert Jaworek <73638117+rbrtjwrk@users.noreply.github.com>
2021-02-22 14:56:33 +0100

    Update and rename rearrange_subject_classification.py to rearrange_subject_attributes.py

---
+++ rearrange_subject_attributes.py
@@ -0,0 +1,27 @@
+import pandas as pd
+
+def rearrange_subject_attributes(dataframe):
+    dataframe=dataframe.copy()
+    jt_col_idx=dataframe.columns.get_loc("Journal_Title")
+    sc_col_idx=dataframe.columns.get_loc("Subject_Classification")
+    sac_col_idx=dataframe.columns.get_loc("Subject_Area_Code")
+    sa_col_idx=dataframe.columns.get_loc("Subject_Area")
+    row_idx=-1
+    for row in dataframe.itertuples():
+        row_idx+=1
+        #rearrange subject classification
+        if len(row.Subject_Classification) > 1:
+            border_1=sum(dataframe.iloc[:row_idx, jt_col_idx]==row.Journal_Title)
+            border_2=sum(dataframe.iloc[row_idx:, jt_col_idx]==row.Journal_Title)-1
+            dataframe.iloc[row_idx, sc_col_idx]=row.Subject_Classification[border_1:(len(row.Subject_Classification)-border_2)]
+        #rearrange subject area code
+        if len(row.Subject_Area_Code) > 1:
+            border_1=sum(dataframe.iloc[:row_idx, jt_col_idx]==row.Journal_Title)
+            border_2=sum(dataframe.iloc[row_idx:, jt_col_idx]==row.Journal_Title)-1
+            dataframe.iloc[row_idx, sac_col_idx]=row.Subject_Area_Code[border_1:(len(row.Subject_Area_Code)-border_2)]
+        #rearrange subject area
+        if len(row.Subject_Area) > 1:
+            border_1=sum(dataframe.iloc[:row_idx, jt_col_idx]==row.Journal_Title)
+            border_2=sum(dataframe.iloc[row_idx:, jt_col_idx]==row.Journal_Title)-1
+            dataframe.iloc[row_idx, sa_col_idx]=row.Subject_Area[border_1:(len(row.Subject_Area)-border_2)]
+    return dataframe
