8da5c1f62bc2e3afba460cc690dc752a250a20f2
Robert Jaworek <73638117+rbrtjwrk@users.noreply.github.com>
2021-03-04 20:39:20 +0100

    Update sjr_rank_per_subject_area_code.py

--- sjr_rank_per_subject_area_code.py
+++ sjr_rank_per_subject_area_code.py
@@ -1,8 +1,13 @@
 import pandas as pd
 import numpy as np
 
+from scopus_harvester.multiply_journals import multiply_journals
+from scopus_harvester.rearrange_subject_attributes import rearrange_subject_attributes
+
 def sjr_rank_per_subject_area_code(dataframe):
     dataframe=dataframe.copy()
+    dataframe=multiply_journals(dataframe)
+    dataframe=rearrange_subject_attributes(dataframe)
     if dataframe["SJR"][0] is not float:
         dataframe["SJR"]=[float(_) for _ in dataframe["SJR"]]
     dataframe["SJR"]=dataframe["SJR"].replace(0., np.nan)
