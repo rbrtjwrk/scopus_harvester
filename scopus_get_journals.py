cdc7c48da3c93eb7a9e8a7eec78b18a579d3d7fd
Robert Jaworek <73638117+rbrtjwrk@users.noreply.github.com>
2022-07-01 15:51:22 +0200

    Update scopus_get_journals.py

--- scopus_get_journals.py
+++ scopus_get_journals.py
@@ -2,6 +2,11 @@ import json
 import requests
 
 def scopus_get_journals(subject_abbrev=None, count=None, start=0):
+    """Retrieves journals from the Scopus Serial Title Metadata API.
+    You can specify subject area abbreviation, count and start index for the retrieval,
+    or you cal leave arguments empty - only count cannot be lower than 1.
+    You have to specify X-ELS-APIKey within the function with your own API key.
+    """
     payload={
             "Accept": "application/json",
             "X-ELS-APIKey": "YOUR_API_KEY"}
@@ -12,3 +17,5 @@ def scopus_get_journals(subject_abbrev=None, count=None, start=0):
                 "start": start}
     response=requests.get("https://api.elsevier.com/content/serial/title", headers=payload, params=parameters)
     return response
+
+
