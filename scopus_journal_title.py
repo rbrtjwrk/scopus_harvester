753f0e0be97fa35b35ba5937db95cd48b56e231b
Robert Jaworek <73638117+rbrtjwrk@users.noreply.github.com>
2022-07-01 15:08:16 +0200

    Update scopus_journal_title.py

--- scopus_journal_title.py
+++ scopus_journal_title.py
@@ -2,7 +2,12 @@ from scopus_harvester.response_to_json import response_to_json
 from scopus_harvester.file_to_data import file_to_data
 
 def scopus_journal_title(response):
+    """Retrieves journal’s title
+    from the API’s response.
+    """
     f=response_to_json(response)
     data=file_to_data(f)
     output=[data[_]["dc:title"] for _ in range(len(data))]
     return output
+
+
