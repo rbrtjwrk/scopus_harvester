b82899d7dfd16933a8a5bcd65d84b1f866d6342b
Robert Jaworek <73638117+rbrtjwrk@users.noreply.github.com>
2022-07-01 16:43:57 +0200

    Update scopus_subject_areas.py

--- scopus_subject_areas.py
+++ scopus_subject_areas.py
@@ -2,6 +2,11 @@ import pandas as pd
 import pkg_resources
 
 def scopus_subject_areas():
+    """Lists Scopus subject areas
+    and their abbreviations.
+    """
     path=pkg_resources.resource_filename("scopus_harvester", "data/scopus_subject_areas.txt")
     output=pd.read_table(path, delimiter="-")
     return output
+
+
