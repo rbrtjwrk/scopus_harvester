9b1043e7db2c2a585ebb4a2c84f966f6b596b540
Robert Jaworek <73638117+rbrtjwrk@users.noreply.github.com>
2022-07-01 15:15:41 +0200

    Update scopus_issn.py

--- scopus_issn.py
+++ scopus_issn.py
@@ -2,11 +2,17 @@ from scopus_harvester.response_to_json import response_to_json
 from scopus_harvester.file_to_data import file_to_data
 
 def parse_scopus_issn_dict(issn_dict):
+    """Parses a dictionary containing multiple issns,
+    when there are more issns per journal.
+    """
     issns=[list(issn_dict[_].values()) for _ in range(len(issn_dict))]
     issns=[issn for list in issns for issn in list]
     return issns
 
 def scopus_issn(response):
+    """Retrieves journal’s issn
+    from the API’s response.
+    """
     output=[]
     f=response_to_json(response)
     data=file_to_data(f)
@@ -25,6 +31,9 @@ def scopus_issn(response):
 
 
 def scopus_eissn(response):
+    """Retrieves journal’s e-issn
+    from the API’s response.
+    """
     output=[]
     f=response_to_json(response)
     data=file_to_data(f)
