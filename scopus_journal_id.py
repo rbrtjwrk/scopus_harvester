4beec3a5792a178121ad8ceb1760cfd3efdec26e
Robert Jaworek <73638117+rbrtjwrk@users.noreply.github.com>
2022-07-01 15:09:43 +0200

    Update scopus_journal_id.py

--- scopus_journal_id.py
+++ scopus_journal_id.py
@@ -2,7 +2,12 @@ from scopus_harvester.response_to_json import response_to_json
 from scopus_harvester.file_to_data import file_to_data
 
 def scopus_journal_id(response):
+    """Retrieves journal’s id
+    from the API’s response.
+    """
     f=response_to_json(response)
     data=file_to_data(f)
     output=[data[_]["source-id"] for _ in range(len(data))]
     return output
+
+
