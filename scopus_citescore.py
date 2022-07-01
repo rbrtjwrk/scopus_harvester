d11bcd923c791a9429cd505fd57bba1cdc557383
Robert Jaworek <73638117+rbrtjwrk@users.noreply.github.com>
2022-07-01 15:17:09 +0200

    Update scopus_citescore.py

--- scopus_citescore.py
+++ scopus_citescore.py
@@ -2,6 +2,9 @@ from scopus_harvester.response_to_json import response_to_json
 from scopus_harvester.file_to_data import file_to_data
 
 def scopus_citescore(response):
+    """Retrieves journal’s Citescore
+    from the API’s response.
+    """
     output=[]
     f=response_to_json(response)
     data=file_to_data(f)
@@ -11,3 +14,5 @@ def scopus_citescore(response):
         else:
             output.append(0.0)
     return output
+
+
