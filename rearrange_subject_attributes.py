a01fbc0840b638b716891d2ef9e258c7bd9a5a9b
Robert Jaworek <73638117+rbrtjwrk@users.noreply.github.com>
2022-07-04 09:17:58 +0200

    Update rearrange_subject_attributes.py

--- rearrange_subject_attributes.py
+++ rearrange_subject_attributes.py
@@ -7,29 +7,27 @@ def rearrange_subject_attributes(dataframe):
     Is executed as a part of processing
     of SJR rak per subject area (code).
     """
-    dataframe=dataframe.copy()
-    jt_col_idx=dataframe.columns.get_loc("Journal_Title")
-    sc_col_idx=dataframe.columns.get_loc("Subject_Classification")
-    sac_col_idx=dataframe.columns.get_loc("Subject_Area_Code")
-    sa_col_idx=dataframe.columns.get_loc("Subject_Area")
-    row_idx=-1
-    for row in dataframe.itertuples():
-        row_idx+=1
+    output=dataframe.copy()
+    jt_col_idx=output.columns.get_loc("Journal_Title")
+    sc_col_idx=output.columns.get_loc("Subject_Classification")
+    sac_col_idx=output.columns.get_loc("Subject_Area_Code")
+    sa_col_idx=output.columns.get_loc("Subject_Area")
+    for i, row in enumerate(output.itertuples(), 0):
         #rearrange subject classification
         if len(row.Subject_Classification) > 1:
-            border_1=sum(dataframe.iloc[:row_idx, jt_col_idx]==row.Journal_Title)
-            border_2=sum(dataframe.iloc[row_idx:, jt_col_idx]==row.Journal_Title)-1
-            dataframe.iloc[row_idx, sc_col_idx]=row.Subject_Classification[border_1:(len(row.Subject_Classification)-border_2)]
+            border_1=sum(output.iloc[:i, jt_col_idx]==row.Journal_Title)
+            border_2=sum(output.iloc[i:, jt_col_idx]==row.Journal_Title)-1
+            output.iloc[i, sc_col_idx]=row.Subject_Classification[border_1:(len(row.Subject_Classification)-border_2)]
         #rearrange subject area code
         if len(row.Subject_Area_Code) > 1:
-            border_1=sum(dataframe.iloc[:row_idx, jt_col_idx]==row.Journal_Title)
-            border_2=sum(dataframe.iloc[row_idx:, jt_col_idx]==row.Journal_Title)-1
-            dataframe.iloc[row_idx, sac_col_idx]=row.Subject_Area_Code[border_1:(len(row.Subject_Area_Code)-border_2)]
+            border_1=sum(output.iloc[:i, jt_col_idx]==row.Journal_Title)
+            border_2=sum(output.iloc[i:, jt_col_idx]==row.Journal_Title)-1
+            output.iloc[i, sac_col_idx]=row.Subject_Area_Code[border_1:(len(row.Subject_Area_Code)-border_2)]
         #rearrange subject area
         if len(row.Subject_Area) > 1:
-            border_1=sum(dataframe.iloc[:row_idx, jt_col_idx]==row.Journal_Title)
-            border_2=sum(dataframe.iloc[row_idx:, jt_col_idx]==row.Journal_Title)-1
-            dataframe.iloc[row_idx, sa_col_idx]=row.Subject_Area[border_1:(len(row.Subject_Area)-border_2)]
-    return dataframe
+            border_1=sum(output.iloc[:i, jt_col_idx]==row.Journal_Title)
+            border_2=sum(output.iloc[i:, jt_col_idx]==row.Journal_Title)-1
+            output.iloc[i, sa_col_idx]=row.Subject_Area[border_1:(len(row.Subject_Area)-border_2)]
+    return output
 
 
