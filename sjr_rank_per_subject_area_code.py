3f0805366c5573d661d542eeba9a9cb57651901e
Robert Jaworek <73638117+rbrtjwrk@users.noreply.github.com>
2022-07-04 09:26:59 +0200

    Update sjr_rank_per_subject_area_code.py

--- sjr_rank_per_subject_area_code.py
+++ sjr_rank_per_subject_area_code.py
@@ -8,15 +8,15 @@ def sjr_rank_per_subject_area_code(dataframe):
     """Computes rank per subject area (code) based on SJR
     for all journals in a given dataframe.
     """
-    dataframe=dataframe.copy()
-    dataframe=multiply_journals(dataframe)
-    dataframe=rearrange_subject_attributes(dataframe)
-    if dataframe["SJR"][0] is not float:
-        dataframe["SJR"]=[float(_) for _ in dataframe["SJR"]]
-    dataframe["SJR"]=dataframe["SJR"].replace(0., np.nan)
-    if dataframe["Subject_Area_Code"][0] is not str:
-        dataframe["Subject_Area_Code"]=[" ".join(_) for _ in dataframe["Subject_Area_Code"]]
-    dataframe["SJR_Rank_per_Subject_Area_Code"]=dataframe.groupby("Subject_Area_Code")["SJR"].rank(ascending=False)
-    return dataframe
+    output=dataframe.copy()
+    output=multiply_journals(output)
+    output=rearrange_subject_attributes(output)
+    if output["SJR"][0] is not float:
+        output["SJR"]=[float(_) for _ in output["SJR"]]
+    output["SJR"]=output["SJR"].replace(0., np.nan)
+    if output["Subject_Area_Code"][0] is not str:
+        output["Subject_Area_Code"]=[" ".join(_) for _ in output["Subject_Area_Code"]]
+    output["SJR_Rank_per_Subject_Area_Code"]=output.groupby("Subject_Area_Code")["SJR"].rank(ascending=False)
+    return output
 
 
