21ee80eca8142c59b08aa4421a8c7a633db64692
Robert Jaworek <73638117+rbrtjwrk@users.noreply.github.com>
2022-07-12 10:18:18 +0200

    Create scopus_open_access.py

---
+++ scopus_open_access.py
@@ -0,0 +1,24 @@
+from scopus_harvester.response_to_json import response_to_json
+from scopus_harvester.file_to_data import file_to_data
+
+def open_access(response):
+    """Returns an indicator
+    0 (is not open access),
+    1 (is open access) or
+    None (no open acces info).
+    """
+    output=[]
+    f=response_to_json(response)
+    data=file_to_data(f)
+    if len(data)>0:
+        for _ in range(len(data)):
+            if data[_]["openaccess"] is None:
+                output.append(None)
+            else:
+                output.append(data[_]["openaccess"])
+    else:
+        output.append(data[0]["openaccess"])
+    return output
+
+  
+  
