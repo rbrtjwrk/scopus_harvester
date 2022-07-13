edf38836ba2e1b87606474ced33c8428002826a5
Robert Jaworek <73638117+rbrtjwrk@users.noreply.github.com>
2022-07-13 10:43:34 +0200

    Create scopus_subject_area_codes.py

---
+++ scopus_subject_area_codes.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import pkg_resources
+
+def scopus_subject_area_codes():
+    """Lists Scopus subject area codes
+    and their subject classifications.
+    """
+    path=pkg_resources.resource_filename("scopus_harvester", "data/scopus_subject_area_codes.txt")
+    output=pd.read_table(path, delimiter="-")
+    return output
+
+  
