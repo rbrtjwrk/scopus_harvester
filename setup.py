a1789795399cfdec1d7f3d97484deadf9479b35a
Robert Jaworek <73638117+rbrtjwrk@users.noreply.github.com>
2022-07-14 10:06:16 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="scopus_harvester",
+    version="0.0.1",
+    author="Robert Jaworek",
+    author_email="jaworek.robert@gmail.com",
+    description="A set of functions to call Scopus Serial Title Metadata API.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/rbrtjwrk/scopus_harvester",
+    project_urls = {
+        "Bug Tracker": "https://github.com/rbrtjwrk/scopus_harvester/issues"
+    },
+    license="MIT",
+    packages=["scopus_harvester"],
+    install_requires=["json", "requests", "pandas", "numpy", "pkg_resources"],
+)
